bf8ae7fb7e31bb43a7048f406c29e6cda5012975
av6387 <98997680+av6387@users.noreply.github.com>
2023-03-25 08:40:59 -0700

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -23,11 +23,9 @@ async def start(c: Client, m: Message):
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         share_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        chat_id = m.from_user.id
-        message_id = message.message_id
 
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
-        await c.edit_message_text(chat_id=chat_id, message_id=message_id,
+        await m.send_message(chat_id=chat_id, message_id=message_id,
               text= "",
        #     quote=True,
             disable_web_page_preview=True,
