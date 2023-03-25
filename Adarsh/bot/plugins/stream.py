af6c873c2c853d02005f79bc2edd54731a02e504
av6387 <98997680+av6387@users.noreply.github.com>
2023-03-25 06:24:54 -0700

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -1,6 +1,7 @@
 #(c) Adarsh-Goel
 import os
 import asyncio
+import pycaption
 from asyncio import TimeoutError
 from Adarsh.bot import StreamBot
 from Adarsh.utils.database import Database
@@ -26,8 +27,8 @@ async def private_receive_handler(c: Client, m: Message):
        
 
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
-        await m.edit_text(
-            text==(previouscaption = ""),
+        await m.edit_message_reply_markup(
+            text="",
        #     quote=True,
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
