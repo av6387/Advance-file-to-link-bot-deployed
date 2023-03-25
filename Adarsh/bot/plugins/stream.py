bdc4af7d3fb72d204bfe60dc01e348da53a1c4bf
av6387 <98997680+av6387@users.noreply.github.com>
2023-03-25 08:54:38 -0700

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -25,8 +25,8 @@ async def start(c: Client, m: Message):
         share_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
 
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
-        await m.edit_text(
-              text= "",
+        video_text = "Original text of the video"
+        await m.edit_text(text=video_text,
        #     quote=True,
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
