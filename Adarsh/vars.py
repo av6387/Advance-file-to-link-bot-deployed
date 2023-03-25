0bba8cef2c91b8964e1459a147e3220496924431
av6387 <98997680+av6387@users.noreply.github.com>
2023-03-25 11:21:25 -0700

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,18 +36,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    if 'DYNO' in environ:
-        ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
-    
-    else:
-        ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
-    if HAS_SSL:
-        SHARE = "https://telegram.me/share/url?url=https://{}/".format(FQDN)
-    else:
-        SHARE = "https://telegram.me/share/url?url=http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', ''))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
