ba6d4d3a56f15ab899e3871adc086162cec56488
av6387 <98997680+av6387@users.noreply.github.com>
2023-03-25 06:52:06 -0700

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -28,7 +28,7 @@ async def private_receive_handler(c: Client, m: Message):
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
         await c.edit_message_reply_markup(
        #     quote=True,
-            disable_web_page_preview=True,
+       #     disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
                [[
                         InlineKeyboardButton("STREAM 🖥", url=stream_link), #Stream Link
