c9dc169c0c14a8ecf1d662617751733a1f35ef06
av6387 <98997680+av6387@users.noreply.github.com>
2023-03-25 09:50:53 -0700

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -26,7 +26,7 @@ async def start(c: Client, m: Message):
 
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
         await m.edit_text(
-              text=msg_text.format((log_msg.caption and log_msg.caption.html),
+              text=msg_text.format(log_msg.caption and log_msg.caption.html),
        #     quote=True,
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
